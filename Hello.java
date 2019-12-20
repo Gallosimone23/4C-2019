225b38d72197d1987a7a595f1ad655d5ed4b1f35
Gallosimone23 <56158532+Gallosimone23@users.noreply.github.com>
2019-12-20 13:29:54 +0100

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eserciziothread;
+
+
+
+/**
+ *
+ * @author gallo.simone
+ */
+public class Hello  extends Thread{
+   
+
+    public Hello() {
+        
+    }
+    
+    
+    public void run(){
+        for (int i = 0; i < 10; i++) {
+           
+            System.out.println("Hello");
+        }
+    }
+
+    
+    
+}
